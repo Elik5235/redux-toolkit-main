28a598507b7fc5dac63dc302340152ca61dcb08a
Elik <eliseyignatchenko06@gmail.com>
2024-07-09 17:28:22 +0300

    data-price-list

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter/Counter";
+import Header from "./components/Header/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+  step: 1
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.count += state.step;
+    },
+    decrement: (state, action) => {
+      state.count -= state.step;
+    },
+    setStep: (state, action) => { // action = { type, payload };
+      state.step = Number(action.payload);
+    }
+  }
+});
+
+const { reducer, actions }  = counterSlice;
+
+// action creators
+const { increment, decrement, setStep } = actions;
+
+export { increment, decrement, setStep };
+export default reducer;
